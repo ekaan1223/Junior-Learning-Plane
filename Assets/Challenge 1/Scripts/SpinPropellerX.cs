650992c3835bc714794740a348ba502cd3eb2c01
ekaan1223 <eneskaangul@gmail.com>
2025-12-18 15:59:06 +0300

    Fully fixed plane

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.forward , 1000 * Time.deltaTime);
+        //transform.forward refers to the z-axis
+        //1000 is the speed of rotation
+
+    }
+}
